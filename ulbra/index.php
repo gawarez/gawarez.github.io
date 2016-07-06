9582d135cffb89f0f3620c174a8e8c236652c5fb
gawarez <gawarez@gmail.com>
2016-07-05 22:04:17 -0300

    importando imc php

---
+++ ulbra/index.php
@@ -0,0 +1,84 @@
+<!DOCTYPE HTML>
+<html lang="pt-br">
+
+<head>
+<meta charset="UTF-8">
+<link rel="stylesheet" type="text/css" href="estilo.css">
+<title>discursiva 2 resposta
+</title>
+</head>
+
+<body>
+
+<table>
+<tr>
+<th>Cálculo de IMC
+</th>
+</tr>
+</table>
+
+<form action="imc.php" method="post">
+
+<table cellspacing="10">
+<tr>
+<th class="th2" colspan="3">Dados Pessoais
+</th>
+</tr>
+<tr>
+<td>Nome completo:
+</td>
+<td><input type="text" name="nomecompleto">
+</td>
+<td>&nbsp;
+</td>
+</tr>
+<tr>
+<td>Data de nascimento:
+</td>
+<td><input type="text" name="datanascimento">
+</td>
+<td>&nbsp;
+</td>
+</tr>
+<tr>
+<td>Sexo:
+</td>
+<td><select name="sexo">
+<option value="feminino">Feminino</option>
+<option value="masculino">Masculino</option>
+</select>
+</td>
+<td>&nbsp;
+</td>
+</tr>
+<tr>
+<td>Peso (kg):
+</td>
+<td><input type="text" name="peso">
+</td>
+<td>&nbsp;
+</td>
+</tr>
+<tr>
+<td>Altura (m):
+</td>
+<td><input type="text" name="altura">
+</td>
+<td>&nbsp;
+</td>
+</tr>
+<tr>
+<td>&nbsp;
+</td>
+<td><input class="input2" type="submit" value="Avaliar">
+</td>
+<td>&nbsp;
+</td>
+</tr>
+</table>
+
+</form> 
+
+</body>
+
+</html>
